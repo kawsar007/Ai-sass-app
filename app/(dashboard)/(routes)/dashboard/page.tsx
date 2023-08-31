cb6ba2320a02e0b005f57bae4e36a3cdd71d3242
kawsar007 <imkawsar007@gmail.com>
2023-08-31 10:49:43 +0600

    Create Layout

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
+
+const DashboardPage = () => {
+  return (
+  <div>
+    <p>Dashboard Page (Protected)</p>
+  </div>
+  );
+};
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div>
+      Landing Page (not protected)
+      <div>
+        <Link href="/sign-in">
+          <Button>Login</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>Register</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
